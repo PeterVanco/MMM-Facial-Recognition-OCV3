1a9c187e1b7bc33bbf0f7c104afa6e286c7e2034
Peter Vanco <petervanco@petervanco.sk>
2018-03-08 13:29:20 +0100

    String format problem fixed?

--- node_helper.js
+++ node_helper.js
@@ -3,6 +3,7 @@
 const NodeHelper = require('node_helper');
 const PythonShell = require('python-shell');
 const {exec} = require('child_process');
+const format = require('string-format');
 
 var pythonStarted = false;
 
@@ -11,6 +12,7 @@ module.exports = NodeHelper.create({
     start: function() {
         // there is no config at this time
         // this.activateMonitor();
+        exec("DISPLAY=:0 xset dpms {} {} {}".format(this.config.stayAwakeAfterMotionStop, this.config.stayAwakeAfterMotionStop, this.config.stayAwakeAfterMotionStop), null);
     },
 
     stop: function() {
